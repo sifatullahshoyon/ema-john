48f73299b1ee8679b2e40bb4384e1d501dee72ad
freelancersifat627e <freelancersifat627@gmail.comm>
2023-09-26 02:12:54 +0600

    compleate cart component calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,17 +6,21 @@ const Cart = ({cart}) => {
     // Option : 2
     // const {cart} = props; 
     let total = 0;
+    let totalShipping = 0;
     for(const product of cart){
         total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = total*7/100;
+    const grandTotal = total + totalShipping + tax;
     return (
         <div className='cart'>
             <h5>Order summary</h5>
             <p>Selected Items: {cart.length}</p>
             <p>Total Price: ${total}</p>
-            <p>Total Shipping Charge: $5</p>
-            <p>Tax: $114</p>
-            <h6>Grand Total: $1559</h6>
+            <p>Total Shipping Charge: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
