f761d674a1752c31144d8d1d538c79ba3cadc25d
freelancersifat627e <freelancersifat627@gmail.comm>
2023-12-15 20:17:09 +0600

    added password show or hide option

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../providers/AuthProviders";
 
 const Login = () => {
     const [error , setError] = useState('');
+    const [show , setShow] = useState(false);
     const {signIn} = useContext(AuthContext);
     const navigate = useNavigate();
     const location = useLocation();
@@ -40,7 +41,14 @@ const Login = () => {
       </div>
       <div className="form-control">
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" required />
+        <input type={show ? "text" : "password"} name="password" id="password" required />
+        <p onClick={() => setShow(!show)}>
+            <small>
+                {
+                    show ? <span>Hide Password</span> : <span>Show Password</span>
+                }
+            </small>
+        </p>
       </div>
       <input className="btn-submit" type="submit" value="Login" />
       <p className="account">
