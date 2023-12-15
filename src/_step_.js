563258ee734d5e83e8bbf2dfce56a4b99e8b40e9
freelancersifat627e <freelancersifat627@gmail.comm>
2023-12-15 20:50:42 +0600

    added step.js file in src folder

---
+++ src/_step_.js
@@ -0,0 +1,44 @@
+/**
+ * 1. Create firebase project
+ * 2. create web app 
+ * 3. npm i firebase
+ * 4. save firebase config and export default app
+ * 5. Build > Authentication > get start > enable sign in method.
+ * 6. Create sign up and login route
+ * 
+ * **/ 
+
+/**
+ * 1. create a context provider file
+ * 2. create a context and set provider
+ * 3. set the children props.
+ * 4. set context value.
+ * 5. set the provider.
+ * 
+ * **/ 
+
+/**
+ * ================= HOSTING ========================
+ * 
+ * --- ONE TIME PER PC/COMPUTER
+ * 
+ * 1. npm install -g firebase-tools
+ * 2. firebase login
+ * 
+ * [ FOR EACH PROJECT ONE TIME ]
+ * 
+ * 1. firebase init
+ * 2. proceed
+ * 3. HOSTING :- firebase (up & down arrow) use space bar . to select
+ * 4. existing project
+ * 5. select the project careful
+ * 6. which project as public directory: [ vite use করলে : dist , create react app use করলে : build ]
+ * 7. single page application : yes
+ * 8. continuous deployment : no
+ * 
+ * [ FOR EVERY TIME DEPLOY ]
+ * 
+ * 1. npm run build
+ * 2. firebase deploy
+ * 
+ * **/ 
